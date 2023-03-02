8fdd8638f3be039e8f461d7b69954346cd1a213b
Ivan <defoxyweex@gmail.com>
2023-03-02 22:02:16 +0300

    Изменем main

--- main.py
+++ main.py
@@ -1,9 +1,1 @@
-def T(matrix: list):
-    for row in range(len(matrix)):
-        for column in range(len(matrix)):
-            matrix[row][column], matrix[column][row] = matrix[column][row], matrix[row][column]
-    return matrix
-
-
-B = [[0, 2, 1], [1, 0, 3], [0, 1, 1]]
-print(T(B))
+print('hello')
