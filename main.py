8a948866c7c2ff83946b2c5aff8a45fff07093aa
Ivan <defoxyweex@gmail.com>
2023-03-02 21:51:41 +0300

    Пушка бомба

---
+++ main.py
@@ -0,0 +1,9 @@
+def T(matrix: list):
+    for row in range(len(matrix)):
+        for column in range(len(matrix)):
+            matrix[row][column], matrix[column][row] = matrix[column][row], matrix[row][column]
+    return matrix
+
+
+A = [[0, 2, 1], [1, 0, 3], [0, 1, 1]]
+print(T(A))
